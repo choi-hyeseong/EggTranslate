a49937fd580baff3bafbb52f6b56f209dfb126ca
choi-hyeseong <bd2845@kakao.com>
2023-12-28 13:42:25 +0900

    refactor: change nested for-loop to stream.

--- src/main/kotlin/com/example/demo/component/ocr/format/BlockFormatter.kt
+++ src/main/kotlin/com/example/demo/component/ocr/format/BlockFormatter.kt
@@ -11,32 +11,55 @@ class BlockFormatter : OCRFormatter {
         return getTextBlocks(response).joinToString(separator = "\n")
     }
 
-    fun getTextBlocks(response: AnnotateImageResponse) : List<String> {
-        val list : MutableList<String> = mutableListOf()
+    fun getTextBlocks(response: AnnotateImageResponse): List<String> {
+        /*
+        val list: MutableList<String> = mutableListOf()
         for (page in response.fullTextAnnotation.pagesList) {
             for (block in page.blocksList) {
                 list.add(getTextInBlock(block))
             }
         }
         return list
+         */
+        return response.fullTextAnnotation.pagesList
+                .flatMap { page -> page.blocksList }
+                .map { block -> getTextInBlock(block) }
+                .toList()
+
     }
 
-    fun getTextInBlock(block : Block) : String {
+    fun getTextInBlock(block: Block): String {
         val builder = StringBuilder()
+        /*
+        첫번째 flatMap -> List<Paragraph>를 List<Word>로
+        두번째 flatMap -> List<Word>를 List<Symbol>로
+        기존 : {PARAGRAPH1, PARAGRAPH2, PARAGRAPH3..} : List<Paragraph>
+        map -> {List<WORD>1, List<WORD>2....} : List<List<Word>>
+        flatMap -> {WORD1-1, WORD1-2, WORD2-1, WORD2-2...} : List<Word>
+        https://dev-kani.tistory.com/33 참조
+         */
+        val symbols = block.paragraphsList
+                .flatMap { paragraph -> paragraph.wordsList }
+                .flatMap { word -> word.symbolsList }
+                .toList()
+        symbols.forEach {
+            builder.append(it.text)
+            if (TextAnnotation.DetectedBreak.BreakType.SPACE == it.property.detectedBreak.type)
+                builder.append(" ")
+        }
+        /*
         for (paragraph in block.paragraphsList) {
             for (word in paragraph.wordsList) {
                 for (symbol in word.symbolsList) {
                     builder.append(symbol.text)
                     if (symbol.property is TextProperty && symbol.property.detectedBreak != null) {
-                        when (symbol.property.detectedBreak.type) {
-                            TextAnnotation.DetectedBreak.BreakType.LINE_BREAK -> builder.append("\n")
-                            TextAnnotation.DetectedBreak.BreakType.SPACE -> builder.append(" ")
-                            else -> {}
-                        }
+                        if (TextAnnotation.DetectedBreak.BreakType.SPACE == symbol.property.detectedBreak.type)
+                            builder.append(" ")
+
                     }
                 }
             }
-        }
+        }*/
         return builder.toString()
     }
 
