28622d76e43a0fcc1dfbfc8fa602e2edcfff7d10
choi-hyeseong <bd2845@kakao.com>
2024-01-02 09:54:01 +0900

    fix: remove new line char in first message.

--- src/main/kotlin/com/example/demo/dto/push/FirebaseResponseDTO.kt
+++ src/main/kotlin/com/example/demo/dto/push/FirebaseResponseDTO.kt
@@ -1,21 +1,21 @@
 package com.example.demo.dto.push
 
-data class FirebaseResponseDTO(var successCount : Int, var failCount : Int, var message : String) {
+data class FirebaseResponseDTO(var successCount: Int, var failCount: Int, var message: String) {
 
     companion object {
-        fun empty() = FirebaseResponseDTO(0,0,"")
+        fun empty() = FirebaseResponseDTO(0, 0, "")
     }
 
     //여러 Response를 묶을 수 있는 함수
-    fun merge(success: Int, fail: Int, content: String) : FirebaseResponseDTO {
+    private fun merge(success: Int, fail: Int, content: String): FirebaseResponseDTO {
         return this.apply {
             successCount += success
             failCount += fail
-            message = message.plus("\n$content")
+            message = if (this.isEmpty()) message else message.plus("\n$content")
         }
     }
 
-    fun merge(responseDTO: FirebaseResponseDTO) : FirebaseResponseDTO  {
+    fun merge(responseDTO: FirebaseResponseDTO): FirebaseResponseDTO {
         return merge(responseDTO.successCount, responseDTO.failCount, responseDTO.message)
     }
 
