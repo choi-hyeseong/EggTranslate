11a7c428b0e13409873bf36bd8a7e7b2d31d8045
choi-hyeseong <bd2845@kakao.com>
2024-01-03 15:19:07 +0900

    feat: add missing parameter exception handler

---
+++ src/main/kotlin/com/example/demo/common/handler/RequestExceptionHandler.kt
@@ -0,0 +1,21 @@
+package com.example.demo.common.handler
+
+import com.example.demo.common.response.Response
+import com.example.demo.logger
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MissingServletRequestParameterException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class RequestExceptionHandler {
+
+    private val log = logger()
+
+    @ExceptionHandler(MissingServletRequestParameterException::class)
+    fun handleMissingParameter(e : MissingServletRequestParameterException) : ResponseEntity<Response<Nothing>> {
+        log.warn("Missing Parameter ${e.parameterName} : ${e.parameterType}")
+        return ResponseEntity(Response.ofFailure("${e.parameterName} 값이 없습니다.", null), HttpStatus.BAD_REQUEST)
+    }
+}
