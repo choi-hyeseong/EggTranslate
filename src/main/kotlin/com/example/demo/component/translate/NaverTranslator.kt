11cae46f82f7f07511d6fcdadbbf226032ce7666
choi-hyeseong <bd2845@kakao.com>
2024-01-03 17:25:58 +0900

    fix: remove annotation in NaverTranslator 현재 안쓰는 구현체이므로 component scan 안되게

--- src/main/kotlin/com/example/demo/component/translate/NaverTranslator.kt
+++ src/main/kotlin/com/example/demo/component/translate/NaverTranslator.kt
@@ -5,7 +5,6 @@ import com.example.demo.dto.translate.TranslateRequestDTO
 import com.example.demo.dto.translate.TranslateResponseDTO
 import org.springframework.stereotype.Component
 
-@Component
 class NaverTranslator(private val naverWebClient: NaverWebClient) : Translator {
     // 테스트 가능성을 위한 WebClient 분리
     // https://velog.io/@ililil9482/%ED%85%8C%EC%8A%A4%ED%8A%B8-%EC%BD%94%EB%93%9C-%EC%9E%91%EC%84%B1 참조
--- src/main/kotlin/com/example/demo/component/web/NaverWebClient.kt
+++ src/main/kotlin/com/example/demo/component/web/NaverWebClient.kt
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import java.net.URLEncoder
 
-@Component
+
 class NaverWebClient {
 
     @Value("\${naver.client-id}")
