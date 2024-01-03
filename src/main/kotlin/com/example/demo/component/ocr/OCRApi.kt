d505b09144274d56ff8902e50b9b3a87296a4f68
choi-hyeseong <bd2845@kakao.com>
2024-01-03 10:12:31 +0900

    feat: support multiple images. 만약 api 호출시 여러 이미지를 한번에 호출가능할경우 override 할것

--- src/main/kotlin/com/example/demo/component/ocr/OCRApi.kt
+++ src/main/kotlin/com/example/demo/component/ocr/OCRApi.kt
@@ -5,4 +5,10 @@ import org.springframework.web.multipart.MultipartFile
 interface OCRApi {
 
     suspend fun readImage(file : MultipartFile) : String
+
+    suspend fun readImage(file : List<MultipartFile>) : List<String> {
+        // java default implementation
+        // 묶음 요청이 될경우 한 api call내에 전부 요청, 아닐경우 하나씩 요청
+        return file.map { readImage(it) }.toList()
+    }
 }
