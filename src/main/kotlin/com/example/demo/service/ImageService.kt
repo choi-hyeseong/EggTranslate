7c5f559a7401a5e822d26546b7060227adc1816f
choi-hyeseong <bd2845@kakao.com>
2024-01-03 14:05:26 +0900

    feat: handle image parallel

--- src/main/kotlin/com/example/demo/service/ImageService.kt
+++ src/main/kotlin/com/example/demo/service/ImageService.kt
@@ -4,8 +4,7 @@ import com.example.demo.common.response.Response
 import com.example.demo.dto.translate.TranslateRequestDTO
 import com.example.demo.dto.translate.TranslateResponseDTO
 import com.example.demo.logger
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
@@ -20,22 +19,36 @@ class ImageService(private val ocrService: OCRService, private val translateServ
 
     suspend fun handleImage(image: List<MultipartFile>): Response<List<TranslateResponseDTO>> {
         //save image
-        image.forEach { saveImage(it) }
-        return translateService.translate(image
-                .map { TranslateRequestDTO("ko", "en", ocrService.readImage(it)) }
-                .toList()
-        )
+        saveImage(image)
+        return coroutineScope {
+            //read parallel logic
+            val requestList = image.map {
+                async {
+                    ocrService.readImage(it)
+                }
+            }.toList()
+            val ocrData = requestList.awaitAll().map { TranslateRequestDTO("ko", "en", it) }.toList()
+            // return translated string
+            translateService.translate(ocrData)
+        }
+
     }
 
-    suspend fun saveImage(image: MultipartFile) {
+    suspend fun saveImage(image: List<MultipartFile>) {
         //이미지 저장하고나서 ocr이 요청될 필요X (parallel하게)
-        withContext(Dispatchers.IO) {
-            val ext = image.originalFilename?.split(".")?.get(1)
-            val path = outPath.plus("\\${System.currentTimeMillis()}.$ext") //value로 형식 부여받기?
-            val outputStream = FileOutputStream(path)
-            outputStream.write(image.bytes)
-            outputStream.close()
-            log.info("image file saved to $path")
+        coroutineScope {
+            image.forEach { image ->
+                launch {
+                    withContext(Dispatchers.IO) {
+                        val ext = image.originalFilename?.split(".")?.get(1)
+                        val path = outPath.plus("\\${System.currentTimeMillis()}.$ext") //value로 형식 부여받기?
+                        val outputStream = FileOutputStream(path)
+                        outputStream.write(image.bytes)
+                        outputStream.close()
+                        log.info("image file saved to $path")
+                    }
+                }
+            }
         }
 
     }
