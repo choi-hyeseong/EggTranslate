6cb9b7257683dcebf309dcc5f5eac4bbf3751451
choi-hyeseong <bd2845@kakao.com>
2024-01-04 17:54:53 +0900

    fix: decouple Exception Handler 기존 Translate와 OCR Handler가 한 클래스내에서 처리. -> 기능별로 분리

--- src/main/kotlin/com/example/demo/ocr/handler/OCRExceptionHandler.kt
+++ src/main/kotlin/com/example/demo/ocr/handler/OCRExceptionHandler.kt
@@ -1,47 +1,33 @@
-package com.example.demo.common.handler
+package com.example.demo.ocr.handler
 
 import com.example.demo.common.response.Response
 import com.example.demo.logger
 import com.example.demo.ocr.exception.AzureRequestException
 import com.example.demo.ocr.exception.GoogleVisionException
-import com.google.api.client.googleapis.json.GoogleJsonResponseException
 import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import org.springframework.web.reactive.function.client.WebClientResponseException
 
 @RestControllerAdvice
-class WebAPIExceptionHandler {
+class OCRExceptionHandler {
 
     private val log = logger()
 
-    @ExceptionHandler(WebClientResponseException::class)
-    protected fun handleWebclientException(e : WebClientResponseException) : ResponseEntity<Response<Nothing>> {
-        printError(e)
-        return buildResponse()
-    }
-
     @ExceptionHandler(GoogleVisionException::class)
-    protected fun handleVisionException(e : GoogleVisionException)  : ResponseEntity<Response<Nothing>>{
+    protected fun handleVisionException(e : GoogleVisionException)  : ResponseEntity<Response<Nothing>> {
         log.error("encountered vision exception : ${e.message}")
         return buildResponse()
     }
 
     @ExceptionHandler(AzureRequestException::class)
-    protected fun handleAzureException(e : AzureRequestException) :ResponseEntity<Response<Nothing>> {
+    protected fun handleAzureException(e : AzureRequestException) : ResponseEntity<Response<Nothing>> {
         log.error("encountered azure exception : ${e.message}")
         return if (e.code == HttpStatus.BAD_REQUEST)
             buildResponse(e.code, e.message ?: "")
         else
             buildResponse()
-    }
-
-    @ExceptionHandler(GoogleJsonResponseException::class)
-    protected fun handleGoogleException(e : GoogleJsonResponseException) : ResponseEntity<Response<Nothing>> {
-        log.error("encountered google translation exception : ${e.details.message}")
-        return buildResponse()
     }
 
     private fun buildResponse() : ResponseEntity<Response<Nothing>> {
@@ -51,11 +37,4 @@ class WebAPIExceptionHandler {
     private fun buildResponse(code: HttpStatusCode, message: String) : ResponseEntity<Response<Nothing>> {
         return ResponseEntity(Response.ofFailure(message, null), code)
     }
-
-    private fun printError(e : WebClientResponseException) {
-        log.error("encountered 400 error(${e.request?.method}) : ${e.message}")
-        log.error("headers : ${e.request?.headers}")
-        log.error("response : ${e.responseBodyAsString}")
-    }
-
 }
