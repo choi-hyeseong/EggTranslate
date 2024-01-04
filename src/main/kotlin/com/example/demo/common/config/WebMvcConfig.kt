5dbf2e9759e2656c8802a2fe86b9115c2c806ac7
choi-hyeseong <bd2845@kakao.com>
2024-01-04 11:19:01 +0900

    feat: apply cors settings.

--- src/main/kotlin/com/example/demo/common/config/WebMvcConfig.kt
+++ src/main/kotlin/com/example/demo/common/config/WebMvcConfig.kt
@@ -2,6 +2,7 @@ package com.example.demo.common.config
 
 import com.example.demo.common.handler.log.LoggingInterceptor
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -12,5 +13,14 @@ class WebMvcConfig(private val loggingInterceptor: LoggingInterceptor) : WebMvcC
 
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(loggingInterceptor).addPathPatterns("/**")
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        // TODO 추후 cors 관련 설정 있을경우 반드시 확인할것.
+        registry.addMapping("/api/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .maxAge(3600)
     }
 }
