87d1619ec3b3ce2f4a4b9c7b942c388dcacb0fe1
choi-hyeseong <bd2845@kakao.com>
2024-01-05 15:11:33 +0900

    chore: edit comment

--- src/main/kotlin/com/example/demo/common/database/converter/AbstractStringFlatter.kt
+++ src/main/kotlin/com/example/demo/common/database/converter/AbstractStringFlatter.kt
@@ -2,7 +2,7 @@ package com.example.demo.common.database.converter
 
 import jakarta.persistence.AttributeConverter
 
-//기존 List형식의 String들을 String으로 직렬화
+//기존 List형식의 T들을 String으로 직렬화
 abstract class AbstractStringFlatter<T> : AttributeConverter<List<T>, String> {
     override fun convertToDatabaseColumn(p0: List<T>?): String {
         return if (p0.isNullOrEmpty())
