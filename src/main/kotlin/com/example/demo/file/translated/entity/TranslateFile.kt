a54121d1ae0858516775bff50bd57a403c5eca74
Silver <leeeun5423@naver.com>
2024-01-08 11:33:57 +0900

    feat : translated_file (DTO, entity, repo) added

--- src/main/kotlin/com/example/demo/file/translated/entity/TranslateFile.kt
+++ src/main/kotlin/com/example/demo/file/translated/entity/TranslateFile.kt
@@ -1,4 +1,18 @@
 package com.example.demo.file.translated.entity
 
-class TranslateFile {
+import jakarta.persistence.*
+import java.io.File
+
+@Entity
+class TranslateFile (
+       @JoinColumn(name = "file_id")
+       var file : File,
+
+       @JoinColumn(name = "translate_result_id")
+       var translate_result : TranslateResult
+
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id : Long = -1
 }
