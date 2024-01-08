c158fa80e4cb47971fe85ac1808ab5a57c85e1ae
Silver <leeeun5423@naver.com>
2024-01-08 11:25:46 +0900

    feat : fileDTO created (not done yet)

--- src/main/kotlin/com/example/demo/file/basic/dto/FileDTO.kt
+++ src/main/kotlin/com/example/demo/file/basic/dto/FileDTO.kt
@@ -1,4 +1,12 @@
 package com.example.demo.file.basic.dto
 
-class FileDTO {
+import com.example.demo.user.basic.dto.UserDto
+
+class FileDTO(
+        val origin_name : String,
+        val save_name : String,
+        val user : UserDto
+        val translate_file : List<TranslateFileRequestDTO>
+){
+
 }
