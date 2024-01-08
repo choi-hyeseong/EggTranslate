cc913c9a36a977d281985b2d3f6c43f2d996b9d0
Silver <leeeun5423@naver.com>
2024-01-08 11:33:57 +0900

    feat : translated_file (DTO, entity, repo) added

--- src/main/kotlin/com/example/demo/file/translated/dto/TranslateFileDTO.kt
+++ src/main/kotlin/com/example/demo/file/translated/dto/TranslateFileDTO.kt
@@ -1,4 +1,10 @@
 package com.example.demo.file.translated.dto
 
+import com.example.demo.file.basic.dto.FileDTO
+
 class TranslateFileDTO {
+    val file : FileDTO,
+    val autoTranslate : autoTranslateDTO
+} {
+
 }
