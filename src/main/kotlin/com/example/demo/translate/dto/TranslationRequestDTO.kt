0457fba7456f333d681c6729d1222b5dca038246
choi-hyeseong <bd2845@kakao.com>
2024-01-09 09:58:10 +0900

    feat: create TranslationRequest test

--- src/main/kotlin/com/example/demo/translate/dto/TranslationRequestDTO.kt
+++ src/main/kotlin/com/example/demo/translate/dto/TranslationRequestDTO.kt
@@ -1,12 +1,38 @@
 package com.example.demo.translate.dto
 
+import com.example.demo.translate.entity.TranslationRequest
+import com.example.demo.translate.type.TranslateState
+import com.example.demo.user.basic.dto.UserDto
+import com.example.demo.user.basic.type.UserType
 import com.example.demo.user.parent.child.dto.ChildRequestDto
 import com.example.demo.user.translator.dto.TranslatorDTO
 
 class TranslationRequestDTO(
-        val status : String,
-        val translator : TranslatorDTO,
-        val autoTranslate : AutoTranslateDTO,
-        val child : ChildRequestDto
+    val id: Long = -1,
+    val user: UserDto,
+    val status: TranslateState,
+    val translator: TranslatorDTO,
+    val autoTranslate: AutoTranslateDTO,
+    val child: ChildRequestDto?
 ) {
+    constructor(translationRequest: TranslationRequest) : this(
+        translationRequest.id,
+        UserDto(translationRequest.user),
+        translationRequest.status,
+        TranslatorDTO(translationRequest.translator),
+        AutoTranslateDTO(translationRequest.autoTranslate),
+        translationRequest.child?.let {
+            ChildRequestDto(it)
+        }
+    )
+
+    fun toEntity(): TranslationRequest = TranslationRequest(
+        id,
+        user.toEntity(),
+        status,
+        user.userType,
+        translator.toEntity(),
+        autoTranslate.toEntity(),
+        child?.toEntity()
+    )
 }
