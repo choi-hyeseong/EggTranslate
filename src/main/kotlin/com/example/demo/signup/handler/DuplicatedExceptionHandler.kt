09f1c5d886d92d87d3a0855e12d5cc7e938b1c34
Silver <leeeun5423@naver.com>
2024-01-11 13:22:00 +0900

    feat : duplicatedExceptionHandler 등록

---
+++ src/main/kotlin/com/example/demo/signup/handler/DuplicatedExceptionHandler.kt
@@ -0,0 +1,12 @@
+package com.example.demo.signup.handler
+
+import com.example.demo.common.response.Response
+import com.example.demo.signup.exception.DuplicatedUsernameException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class DuplicatedExceptionHandler {
+    @ExceptionHandler(DuplicatedUsernameException::class)
+    fun handleDuplicatedUsername(e : DuplicatedUsernameException) = Response.ofFailure(e.message, null)
+}
