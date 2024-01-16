897b69f4b6e2a74909ba3df150772a4b9b81a227
choi-hyeseong <bd2845@kakao.com>
2024-01-16 09:23:05 +0900

    fix: ignore id field on json file. id는 auto increase이기 때문에 수정이 아닌 이상 id값은 null이 되어야 select 쿼리가 안나감.

--- src/main/kotlin/com/example/demo/voca/dto/VocaDTO.kt
+++ src/main/kotlin/com/example/demo/voca/dto/VocaDTO.kt
@@ -2,12 +2,13 @@ package com.example.demo.voca.dto
 
 import com.example.demo.voca.entity.Voca
 import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 //json creator가 있어야 생성자 인식가능
 class VocaDTO @JsonCreator constructor (
-    @JsonProperty(value = "id")
-    val id : Long?,
+
     @JsonProperty(value = "lang")
     val lang : String,
     @JsonProperty(value = "origin")
@@ -15,8 +16,12 @@ class VocaDTO @JsonCreator constructor (
     @JsonProperty(value = "translate")
     val translate : String
 ) {
+    //json내에 id값이 있어선 안됨. (auto increase라 지정안해두면 알아서 로드 됨.)
+    private var id : Long? = null
 
-    constructor(voca: Voca) : this(voca.id, voca.lang, voca.origin, voca.translate)
+    constructor(voca: Voca) : this(voca.lang, voca.origin, voca.translate) {
+        this.id = voca.id
+    }
 
-    fun toEntity() : Voca = Voca(id, lang, origin, translate)
+    fun toEntity() : Voca = Voca(id, lang, origin, translate) //null로 안해두면 select 발생함. 굳이 엔티티 저장할때는 id값 필요 없음 (리셋됨)
 }
