3b36cae2e6b592ce510885e16af88b0f47da991c
choi-hyeseong <bd2845@kakao.com>
2024-01-19 16:05:25 +0900

    chore: add comment

--- src/main/kotlin/com/example/demo/ocr/component/ocr/OCRApi.kt
+++ src/main/kotlin/com/example/demo/ocr/component/ocr/OCRApi.kt
@@ -4,6 +4,9 @@ import org.springframework.web.multipart.MultipartFile
 
 interface OCRApi {
 
+    //read image할때 table 가져오는 response 구성하기
+    //즉 기존 OCRAPI를 상속받는 TableOCRAPI를 만들고, readTable을 만듬
+    //readImage는 readTable.content로 반환.
     suspend fun readImage(file : MultipartFile) : String
 
     suspend fun readImage(file : List<MultipartFile>) : List<String> {
--- src/main/kotlin/com/example/demo/translate/web/prehandle/replacer/TranslatePreReplacer.kt
+++ src/main/kotlin/com/example/demo/translate/web/prehandle/replacer/TranslatePreReplacer.kt
@@ -2,6 +2,13 @@ package com.example.demo.translate.web.prehandle.replacer
 
 interface TranslatePreReplacer {
 
+    /*
+    * OcrPostReplacer랑 다른게 뭐냐라고 생각하면 파라미터 한개 차이일뿐.
+    * 따라서 OcrPostReplacer를 공용 인터페이스로 바꾸고 이를 구현해도 됨.
+    * 그러면 이 인터페이스는 생성자에다 lang만 넣어주면 위 replace메소드를 그대로 쓸 수 있음.
+    * 근데 각 언어별로 Replacer가 생성되는게 맞는건가? 같은 '언어'를 받아서 동일한 로직에 따라 바꿔주는건데
+    * 그래서 PostReplacer와 PreReplacer를 분리하였음. 더 좋은 방식이 생각이 안남.
+     */
     fun replace(lang : String, input : String) : String
 
 }
