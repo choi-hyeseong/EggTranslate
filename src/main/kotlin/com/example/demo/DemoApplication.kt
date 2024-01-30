decfdcbf13087711ebedb045be56322a2965e58a
choi-hyeseong <bd2845@kakao.com>
2024-01-30 16:08:17 +0900

    comment: add comment in extension fun

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -20,6 +20,7 @@ fun main(args: Array<String>) {
 inline fun <reified T> T.logger() = LoggerFactory.getLogger(T::class.java)!!
 
 // inline을 안시키면 코드가 복사되서 진행되는 방식이 아니라 suspend에서도 동작하지 못함.
+//nullable한 객체에서 사용 가능하며, 해당 객체가 null일경우 null리턴, null이 아닐경우 이후에 나오는 블럭의 결과값 리턴.
 inline fun <reified Any, R> Any?.convertOrNull(block : (Any) -> R) : R? {
 	return if (this == null)
 		null
