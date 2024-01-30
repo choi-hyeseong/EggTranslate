31bea0bde11b606f94e6b73c4901a13239491afb
choi-hyeseong <bd2845@kakao.com>
2024-01-30 10:31:17 +0900

    feat: add usertype (admin, guest)

--- src/main/kotlin/com/example/demo/user/basic/type/UserType.kt
+++ src/main/kotlin/com/example/demo/user/basic/type/UserType.kt
@@ -2,6 +2,6 @@ package com.example.demo.user.basic.type
 
 enum class UserType {
                     //request 요청시 user type 구분하는데 사용하는 타입 enum
-    PARENT, TEACHER, TRANSLATOR
+    PARENT, TEACHER, TRANSLATOR, ADMIN, GUEST
 
 }
