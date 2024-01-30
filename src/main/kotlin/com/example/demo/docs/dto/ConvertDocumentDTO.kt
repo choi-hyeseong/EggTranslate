032f036f7a45eaaf9fb44b70dc7b5b6c8c5d0441
choi-hyeseong <bd2845@kakao.com>
2024-01-30 17:40:14 +0900

    feat: add inherit AbstractDTO file 리소스 변환을 위한 상속.

--- src/main/kotlin/com/example/demo/docs/dto/ConvertDocumentDTO.kt
+++ src/main/kotlin/com/example/demo/docs/dto/ConvertDocumentDTO.kt
@@ -3,15 +3,16 @@ package com.example.demo.docs.dto
 import com.example.demo.convertOrNull
 import com.example.demo.docs.entity.ConvertDocument
 import com.example.demo.docs.type.DocumentType
+import com.example.demo.file.dto.AbstractFileDTO
 import com.example.demo.user.basic.dto.UserDto
 import com.example.demo.user.basic.entity.User
 
 class ConvertDocumentDTO(
     val id: Long?,
     val type: DocumentType,
-    val savePath: String,
+    savePath: String,
     val userDto: UserDto?
-) {
+) : AbstractFileDTO(savePath) {
     constructor(convertDocument: ConvertDocument) : this(
         convertDocument.id,
         convertDocument.type,
--- src/main/kotlin/com/example/demo/docs/dto/DocumentDTO.kt
+++ src/main/kotlin/com/example/demo/docs/dto/DocumentDTO.kt
@@ -3,6 +3,7 @@ package com.example.demo.docs.dto
 import com.example.demo.convertOrNull
 import com.example.demo.docs.entity.Document
 import com.example.demo.docs.type.DocumentType
+import com.example.demo.file.dto.AbstractFileDTO
 import com.example.demo.user.basic.dto.UserDto
 import com.example.demo.user.basic.entity.User
 
@@ -10,9 +11,9 @@ class DocumentDTO(
     val id: Long?,
     val type: DocumentType,
     val originName: String,
-    val savePath: String,
+    savePath: String,
     val userDto: UserDto?
-) {
+) : AbstractFileDTO(savePath) {
     constructor(document: Document) : this(
         document.id,
         document.type,
