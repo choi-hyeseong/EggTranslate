3e97064a5909fa96d0a383b4ab62f39bee9dc22c
choi-hyeseong <bd2845@kakao.com>
2024-01-31 13:44:45 +0900

    feat: support download file.

--- src/main/kotlin/com/example/demo/file/controller/FileController.kt
+++ src/main/kotlin/com/example/demo/file/controller/FileController.kt
@@ -3,6 +3,7 @@ package com.example.demo.file.controller
 import com.example.demo.file.dto.FileDTO
 import com.example.demo.file.dto.FileRequestDTO
 import com.example.demo.file.service.FileService
+import com.example.demo.file.util.FileUtil
 import org.springframework.core.io.Resource
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
@@ -13,15 +14,25 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.URLEncoder
 
 @RestController
 @RequestMapping("/api/file")
 class FileController(private val fileService: FileService) {
 
     @GetMapping("/{id}")
-    suspend fun getFile(@PathVariable(value = "id") id: Long): ResponseEntity<Resource> {
+    suspend fun showFile(@PathVariable(value = "id") id: Long): ResponseEntity<Resource> {
         return ResponseEntity(fileService.getFile(id), HttpHeaders().apply {
             add("Content-Type", "image/jpeg")
+        }, HttpStatus.OK)
+    }
+
+    @GetMapping("/download/{id}")
+    suspend fun downloadFile(@PathVariable(value = "id") id: Long): ResponseEntity<Resource> {
+        val file = fileService.findFileById(id)
+        val filename = URLEncoder.encode(file.originName, "UTF-8")
+        return ResponseEntity(FileUtil.convertFileToResource(file), HttpHeaders().apply {
+            add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=$filename")
         }, HttpStatus.OK)
     }
 
