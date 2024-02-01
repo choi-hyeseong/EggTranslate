94176e5f648890ff16f6f2678e953d525f8efbfe
choi-hyeseong <bd2845@kakao.com>
2024-02-01 11:12:16 +0900

    comment: comment in pageable

--- src/main/kotlin/com/example/demo/common/page/Pageable.kt
+++ src/main/kotlin/com/example/demo/common/page/Pageable.kt
@@ -3,7 +3,7 @@ package com.example.demo.common.page
 data class Pageable<T> (
     //현재 페이지
     val current : Int,
-    //최대 페이지
+    //최대 페이지, 실제 쿼리에 사용되는 인덱스를 나타냈음. 만약 2페이지면 0,1 페이지에 접근가능하고, max는 1로 설정됨.
     val max : Int,
     //내용
     val content : List<T>,
