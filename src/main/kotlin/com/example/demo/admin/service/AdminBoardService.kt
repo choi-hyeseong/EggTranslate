2c39fa202113fa142e50749567d7a61fd536ba7d
choi-hyeseong <bd2845@kakao.com>
2024-02-02 14:44:10 +0900

    feat: add transactional

--- src/main/kotlin/com/example/demo/admin/service/AdminBoardService.kt
+++ src/main/kotlin/com/example/demo/admin/service/AdminBoardService.kt
@@ -6,22 +6,24 @@ import com.example.demo.board.dto.BoardResponseDTO
 import com.example.demo.board.service.BoardService
 import com.example.demo.file.dto.FileDTO
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AdminBoardService(
     private val boardService: BoardService
 ) {
 
+    @Transactional
     suspend fun write( id : Long,  fileDto: List<FileDTO>, boardRequestDTO: BoardRequestDTO,) : BoardResponseDTO {
         return  boardService.writeBoard(id, fileDto, boardRequestDTO)
     }
 
+    @Transactional
     suspend fun edit( id : Long,  boardEditDTO: BoardEditRequestDTO) : BoardResponseDTO {
         return  boardService.editBoard(boardEditDTO.toEditDTO(id))
     }
 
-
-
+    @Transactional
     suspend fun delete( id : Long) : BoardResponseDTO {
         return  boardService.deleteBoard(id)
     }
