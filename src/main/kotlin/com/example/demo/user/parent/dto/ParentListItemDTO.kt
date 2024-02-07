8198084a33ea8e1ecbeb0db722826a7e83e3f1f9
choi-hyeseong <bd2845@kakao.com>
2024-02-07 10:03:55 +0900

    feat: add name field in list item

--- src/main/kotlin/com/example/demo/user/parent/dto/ParentListItemDTO.kt
+++ src/main/kotlin/com/example/demo/user/parent/dto/ParentListItemDTO.kt
@@ -6,7 +6,8 @@ import com.example.demo.user.parent.entity.Parent
 class ParentListItemDTO (
     val id : Long?,
     val userId : Long?,
+    val name : String,
     var children : List<ChildListItemDTO>,
 ) {
-    constructor(parent : Parent) : this(parent.id, parent.user.id, parent.children.map { ChildListItemDTO(it) })
+    constructor(parent : Parent) : this(parent.id, parent.user.id, parent.user.name, parent.children.map { ChildListItemDTO(it) })
 }
