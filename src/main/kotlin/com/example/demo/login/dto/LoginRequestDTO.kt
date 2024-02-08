0814eeea7042c6a04329ab3329d4f439cacf7d7a
choi-hyeseong <bd2845@kakao.com>
2024-02-08 09:13:29 +0900

    feat: remove unused login

--- src/main/kotlin/com/example/demo/login/dto/LoginRequestDTO.kt
+++
@@ -1,6 +0,0 @@
-package com.example.demo.login.dto
-
-data class LoginRequestDTO(
-        val username: String,
-        val password: String
-)
--- src/main/kotlin/com/example/demo/login/dto/LoginResponseDTO.kt
+++
@@ -1,5 +0,0 @@
-package com.example.demo.login.dto
-
-data class LoginResponseDTO(
-        val accessToken: String
-)
