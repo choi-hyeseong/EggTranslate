424872910ca8e468c6ea7f34aed0f8105baaa51e
choi-hyeseong <bd2845@kakao.com>
2024-02-16 20:10:15 +0900

    feat: divide into function & add try catch to result

--- src/main/kotlin/com/example/demo/ocr/component/ocr/azure/AzureClient.kt
+++ src/main/kotlin/com/example/demo/ocr/component/ocr/azure/AzureClient.kt
@@ -42,46 +42,48 @@ class AzureClient(private val asyncDocumentAnalysis: DocumentAnalysisAsyncClient
     suspend fun requestImage(file: MultipartFile): AzureResponseDTO {
         //stream이 아닌 byte array로 받아와서 여러군데서 써도 문제 없음
         val documentData = BinaryData.fromBytes(file.bytes)
+        try {
+            //AsyncPoller를 가져옴 (PollInterval은 0.25s)
+            val request: PollerFlux<OperationResult, AnalyzeResult> =
+                asyncDocumentAnalysis.beginAnalyzeDocument("prebuilt-layout", documentData).apply { setPollInterval(Duration.ofMillis(pollIntervalMillis)) }
+            //결과값을 mapping함 (flatmap으로 평탄화) 언제? : 결과를 받았을때
+            val response = parseFluxRequest(request)
+            return withContext(Dispatchers.IO) { response.blockFirst()!! } //실제 webflux를 사용하는게 아니므로 최종적인 block
+        } catch (e: Exception) {
+            if (e is AzureRequestException)
+                throw e
+            else
+                throw AzureRequestException(HttpStatus.INTERNAL_SERVER_ERROR, e.message ?: e.localizedMessage)
+        }
+    }
+}
 
-        //AsyncPoller를 가져옴 (PollInterval은 0.25s)
-        val request: PollerFlux<OperationResult, AnalyzeResult> =
-            asyncDocumentAnalysis.beginAnalyzeDocument("prebuilt-layout", documentData).apply {
-                setPollInterval(Duration.ofMillis(pollIntervalMillis))
+private fun parseFluxRequest(request: PollerFlux<OperationResult, AnalyzeResult>): Flux<AzureResponseDTO> {
+    return request.flatMap {
+        val result = it.finalResult
+        val status: LongRunningOperationStatus = it.status
+        if (status == LongRunningOperationStatus.FAILED)
+        //실패했을경우 Mono의 Error로 Exception을 반환
+            Mono.error(AzureRequestException(HttpStatus.INTERNAL_SERVER_ERROR, "failed to request."))
+        else
+        //성공한경우 Response 파싱. FlatMap이므로 Mono<Mono<~>> 가 아닌 Mono로 가져올 수 있음 (평탄화)
+            result.flatMap { res ->
+                if (res.paragraphs.isNullOrEmpty())
+                //문단이 비어있을경우 Mono의 Error로 파싱
+                    Mono.error(AzureRequestException(HttpStatus.BAD_REQUEST, "인식된 텍스트가 없습니다."))
+                else
+                //성공했을경우 Mono의 just로 Mono로 파싱후 반환
+                    Mono.just(AzureResponseDTO(res.content, parseParagraph(res)))
             }
-        //결과값을 mapping함 (flatmap으로 평탄화) 언제? : 결과를 받았을때
-        val response = request.flatMap {
-            // TODO Timeout Handling 안됨
-            val result = it.finalResult
-            val status: LongRunningOperationStatus = it.status
-            if (status == LongRunningOperationStatus.FAILED)
-                //실패했을경우 Mono의 Error로 Exception을 반환
-                Mono.error(AzureRequestException(HttpStatus.INTERNAL_SERVER_ERROR, "failed to request."))
-            else
-                //성공한경우 Response 파싱. FlatMap이므로 Mono<Mono<~>> 가 아닌 Mono로 가져올 수 있음 (평탄화)
-                result.flatMap { res ->
-                    if (res.paragraphs.isNullOrEmpty())
-                        //문단이 비어있을경우 Mono의 Error로 파싱
-                        Mono.error(AzureRequestException(HttpStatus.BAD_REQUEST, "인식된 텍스트가 없습니다."))
-                    else
-                        //성공했을경우 Mono의 just로 Mono로 파싱후 반환
-                        Mono.just(AzureResponseDTO(res.content, parseParagraph(res)))
-                }
-        }.doOnError { //에러가 발생했을경우 (flatMap이 실행된 다음 Flux-Mono Exception이 있는경우 doOnError가 호출됨!! 따라서 doOnError를 마지막쯤 위치)
-            if (it is AzureRequestException)
-                throw it
-            else
-                throw AzureRequestException(HttpStatus.INTERNAL_SERVER_ERROR, it.message ?: it.localizedMessage)
-        }
-        return withContext(Dispatchers.IO) { response.blockFirst()!! } //실제 webflux를 사용하는게 아니므로 최종적인 block
     }
+}
 
-    private fun parseParagraph(result: AnalyzeResult): List<Paragraph> {
-        val paragraphList: MutableList<Paragraph> = mutableListOf()
-        result.paragraphs.forEach {
-            val polygons = it.boundingRegions[0].boundingPolygon
-            polygons.sortWith { p1: Point, p2: Point -> (p1.x + p1.y).compareTo((p2.x + p2.y)) }
-            paragraphList.add(Paragraph(it.content, Area(polygons[0], polygons[3])))
-        }
-        return paragraphList
+private fun parseParagraph(result: AnalyzeResult): List<Paragraph> {
+    val paragraphList: MutableList<Paragraph> = mutableListOf()
+    result.paragraphs.forEach {
+        val polygons = it.boundingRegions[0].boundingPolygon
+        polygons.sortWith { p1: Point, p2: Point -> (p1.x + p1.y).compareTo((p2.x + p2.y)) }
+        paragraphList.add(Paragraph(it.content, Area(polygons[0], polygons[3])))
     }
+    return paragraphList
 }
